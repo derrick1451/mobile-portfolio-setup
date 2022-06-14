5b52a156dcab3cbc8a97db5c101c92232773cd73
Derrick1451 <haylederrick14@gmail.com>
2022-06-14 15:35:42 +0300

    edit css files

--- script.js
+++ script.js
@@ -4,18 +4,16 @@ const closeIcon = document.querySelector('.close-icon');
 const navLinks = document.querySelectorAll('.nav-links');
 
 hamburger.addEventListener('click', () => {
-    popUp.classList.toggle('active-pop-up');
-    closeIcon.classList.toggle('close');
+popUp.classList.toggle('active-pop-up');
+closeIcon.classList.toggle('close');
 });
 closeIcon.addEventListener('click', () => {
-    popUp.classList.toggle('active-pop-up');
-    closeIcon.classList.toggle('close');
+  popUp.classList.toggle('active-pop-up');
+  closeIcon.classList.toggle('close');
 });
 navLinks.forEach((nav) => {
-    nav.addEventListener('click', () => {
-        popUp.classList.remove('active-pop-up');
-        closeIcon.classList.remove('close');
-    });
-  });
-
-
+  nav.addEventListener('click', () => {
+    popUp.classList.remove('active-pop-up');
+    closeIcon.classList.remove('close');
+});
+});
