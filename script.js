873c64c2221a6f5f391855226f741f29e49c9d98
Derrick1451 <haylederrick14@gmail.com>
2022-06-14 15:30:47 +0300

    edit javascript files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let hamburger = document.querySelector('.loose');
-let popUp = document.querySelector('.pop-up');
-let closeIcon = document.querySelector('.close-icon');
-let navLinks = document.querySelectorAll('.nav-links')
+const hamburger = document.querySelector('.loose');
+const popUp = document.querySelector('.pop-up');
+const closeIcon = document.querySelector('.close-icon');
+const navLinks = document.querySelectorAll('.nav-links');
 
 hamburger.addEventListener('click', () => {
     popUp.classList.toggle('active-pop-up');
